e9300570be8bb6c8c9f9b64cffbcd44c707cc013
vuongbachdoan <vuongbachdoan@gmail.com>
2023-03-22 11:24:50 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import { Provider } from "react-redux";
+import store from './Redux/store';
+import "./Assets/scss/_base_.scss";
+import "./Assets/scss/style.scss";
+import "./index.css";
+import { Authenticator } from '@aws-amplify/ui-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <Authenticator.Provider>
+          <RouterProvider router={router}/>
+        </Authenticator.Provider>
+      </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
